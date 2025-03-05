0495c4a28865559c1b9e0586202d5301209fe709
tinh <tinhtranquocx@gmail.com>
2025-03-05 15:36:44 +0700

    update db , api kem

--- IcreamShopApi/Data/CreamDbContext.cs
+++ IcreamShopApi/Data/CreamDbContext.cs
@@ -1,16 +1,19 @@
-﻿namespace IcreamShopApi.Data
+﻿using IcreamShopApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IcreamShopApi.Data
 {
-	using IcreamShopApi.Models;
-	using Microsoft.EntityFrameworkCore;
+	public class CreamDbContext : DbContext 
 
-	public class AppDbContext : DbContext
 	{
-		public AppDbContext() { }
-		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+
+		public CreamDbContext() { }
+		public CreamDbContext(DbContextOptions<CreamDbContext> options) : base(options) { }
 		public DbSet<User> Users { get; set; }
 		public DbSet<IceCream> IceCreams { get; set; }
 		public DbSet<Cart> Carts { get; set; }
+		public DbSet<Category> Categories { get; set; }
 		public DbSet<Order> Orders { get; set; }
 		public DbSet<OrderDetail> OrderDetails { get; set; }
 		public DbSet<Review> Reviews { get; set; }
@@ -33,7 +36,7 @@
 				.HasOne(c => c.User)
 				.WithMany()
 				.HasForeignKey(c => c.UserId)
-				.OnDelete(DeleteBehavior.Cascade);
+				.OnDelete(DeleteBehavior.Restrict);
 
 			modelBuilder.Entity<OrderDetail>()
 				.HasOne(od => od.Order)
@@ -45,8 +48,22 @@
 				.HasOne(r => r.User)
 				.WithMany(u => u.Reviews)
 				.HasForeignKey(r => r.UserId)
-				.OnDelete(DeleteBehavior.Cascade);
+				.OnDelete(DeleteBehavior.Restrict);
+			
+			
+
+
+			modelBuilder.Entity<Category>().HasData(
+	  new Category { CategoryId = 1, Name = "Chocolate", image = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Chocolate.jpg/1155px-Chocolate.jpg" }
+
+  );
+			modelBuilder.Entity<IceCream>().HasData(
+		new IceCream { IceCreamId = 1, Name = "Chocolate Ice Cream", Price = 10.99M, Stock = 100, CategoryId = 1, ImageUrl = "choco.jpg", Description = "kem xin" }
+	);
 		}
+
 	}
 
 }
+
+
---
+++ IcreamShopApi/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IcreamShopApi.Models
+{
+	public class Category
+	{
+		public int CategoryId { get; set; }
+		public string Name { get; set; }
+		public string image {  get; set; }
+
+		// Tránh lỗi vòng lặp JSON khi trả về danh sách IceCreams
+		[JsonIgnore]
+		public List<IceCream> IceCreams { get; set; } = new List<IceCream>();
+
+	}
+}
